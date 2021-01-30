e47c0f4fe7821c0077e82d2bb663b3b565774ec6
Jesus Vasquez <jesusvasquez333@gmail.com>
2021-01-30 11:43:48 -0800

    Fix Flake 8 errors and warnings

--- atca_monitor.py
+++ atca_monitor.py
@@ -3,23 +3,23 @@
 import getopt
 import subprocess
 import os
-import time
 import sys
 import logging
 import rogue
 import pyrogue
 import pyrogue.protocols.epics
 
-# Setup the logger level to 'Error' by default
-logger = logging.getLogger('pyrogue')
-logger.setLevel(rogue.Logging.Error)
-
 # Import the ATCA IPMI monitor (Static or DYnamic version)
-#from atcaipmi.monitor import AtcaIpmiDynamicMonitor as AtcaIpmiMonitor
+# from atcaipmi.monitor import AtcaIpmiDynamicMonitor as AtcaIpmiMonitor
 from atcaipmi.monitor import AtcaIpmiStaticMonitor as AtcaIpmiMonitor
 
 # Import the Root, which includes a device for the while ATCA crate
 from atcaipmi.atca_root import AtcaCrateRoot
+
+# Setup the logger level to 'Error' by default
+logger = logging.getLogger('pyrogue')
+logger.setLevel(rogue.Logging.Error)
+
 
 def usage(name):
     """
@@ -31,26 +31,34 @@ def usage(name):
     Returns:
         None.
     """
-    print("Usage: {} -S|--shelfmanager <shelfmanager> [-e|--epics prefix] [-g|--gui] [-h|--help]".format(name))
+    print("Usage: {} -S|--shelfmanager <shelfmanager> "
+          "[-e|--epics prefix] [-g|--gui] [-h|--help]".format(name))
     print("")
-    print("    -S|--shelfmanager <shelfmanager> : Node name of the ATCA shelfmanager.")
-    print("    -e|--epics        <prefix>       : Start an EPICS server using <prefix> as the PV name prefix.")
-    print("                                       (default: the shelfmanager node name)")
-    print("    -g|--gui                         : Start the server with a GUI.")
-    print("    -h|--help                        : Show this message")
+    print("    -S|--shelfmanager <shelfmanager> : "
+          "Node name of the ATCA shelfmanager.")
+    print("    -e|--epics        <prefix>       : "
+          "Start an EPICS server using <prefix> as the PV name prefix.")
+    print("                                       "
+          "(default: the shelfmanager node name)")
+    print("    -g|--gui                         : "
+          "Start the server with a GUI.")
+    print("    -h|--help                        : "
+          "Show this message")
     print("")
     print("")
+
 
 if __name__ == "__main__":
 
-    shelfmanager=""
-    epics_prefix=""
-    use_gui=False
+    shelfmanager = ""
+    epics_prefix = ""
+    use_gui = False
     # Read Arguments
     try:
-        opts, _ = getopt.getopt(sys.argv[1:],
-                "S:e:gh",
-                ["shelfmanager","epics","gui","help"])
+        opts, _ = getopt.getopt(
+            sys.argv[1:],
+            "S:e:gh",
+            ["shelfmanager", "epics", "gui", "help"])
     except getopt.GetoptError:
         usage(sys.argv[0])
         sys.exit()
@@ -74,18 +82,21 @@ if __name__ == "__main__":
     # Check if shelfmanager is online
     print("Trying to ping the shelfmanager '{}'...".format(shelfmanager))
     try:
-       dev_null = open(os.devnull, 'w')
-       subprocess.check_call(["ping", "-c2", shelfmanager], stdout=dev_null, stderr=dev_null)
-       print("    shelfmanager is online")
-       print("")
+        dev_null = open(os.devnull, 'w')
+        subprocess.check_call(
+            ["ping", "-c2", shelfmanager],
+            stdout=dev_null,
+            stderr=dev_null)
+        print("    shelfmanager is online")
+        print("")
     except subprocess.CalledProcessError:
-       print("    ERROR: shelfmanager can't be reached!")
-       exit()
+        print("    ERROR: shelfmanager can't be reached!")
+        exit()
 
     # Use the shelfmanager as EPICS prefix, unless a different one
     # was defined by the user
     if not epics_prefix:
-        epics_prefix=shelfmanager
+        epics_prefix = shelfmanager
 
     # Start the ATCA IPMI monitor
     ipmi = AtcaIpmiMonitor(shelfmanager=shelfmanager)
